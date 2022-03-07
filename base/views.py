e2feccb4b6c6710b032e593a473e633de5cefef3
kiprotichdominic <kiprotichdominic102@gmail.com>
2022-03-07 09:00:31 +0300

    add base template to ease template resusability

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from base.models import Task
 
 
@@ -12,3 +13,6 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
+
+class TaskCreate(CreateView):
+    pass
