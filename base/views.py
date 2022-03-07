c5e4a4b4ad4a7a26f32120dd8cbeaf1e3ba05e09
kiprotichdominic <kiprotichdominic102@gmail.com>
2022-03-07 08:37:31 +0300

    customize template rendring for class based views

--- base/views.py
+++ base/views.py
@@ -6,6 +6,9 @@ from base.models import Task
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
+    template_name = 'base/tasks.html'
 
 class TaskDetail(DetailView):
     model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
