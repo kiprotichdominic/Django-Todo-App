c33aa19e4ee429b346fbb4136f09fb5465d78c47
kiprotichdominic <kiprotichdominic102@gmail.com>
2022-03-07 10:17:26 +0300

    add fucntion to filter data according to user

--- base/views.py
+++ base/views.py
@@ -23,6 +23,12 @@ class TaskList(LoginRequiredMixin,ListView):
     context_object_name = 'tasks'
     template_name = 'base/tasks.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False)
+        return context
+
 class TaskDetail(LoginRequiredMixin,DetailView):
     model = Task
     context_object_name = 'task'
